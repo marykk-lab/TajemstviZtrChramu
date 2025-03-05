caa6386fff6b43008df8b77b1125bcf3ad80dceb
marykk-lab <maksimos0899@gmail.com>
2025-03-05 11:56:59 +0100

    pribral tridy mistnosti

---
+++ src/Rooms.java
@@ -0,0 +1,11 @@
+public class Rooms extends Command{
+    @Override
+    public String execute() {
+        return "";
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}
